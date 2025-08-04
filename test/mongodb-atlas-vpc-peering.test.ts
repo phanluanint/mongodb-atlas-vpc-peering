de2e5352702374cae1adc3e9b503f519234550c4
Luan Phan <phanluanint@gmail.com>
2025-08-04 12:00:58 +0700

    Initial commit

---
+++ test/mongodb-atlas-vpc-peering.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MongodbAtlasVpcPeering from '../lib/mongodb-atlas-vpc-peering-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/mongodb-atlas-vpc-peering-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MongodbAtlasVpcPeering.MongodbAtlasVpcPeeringStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
